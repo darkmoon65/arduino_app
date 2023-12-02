d139d450b357d2bf4d700d1594c5ad76a193b426
Cesar Alonso Santillana Q <csantillanaq@ulasalle.edu.pe>
2023-12-02 14:56:30 -0500

    back

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+import serial 
+import time 
 
 app = Flask(__name__) #instancia
+arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200) 
 
-@app.route('/') #wrap (decorator)
-def index():
-    return 'Hola mundo' #return string
+@app.route('/prender', methods = ['POST'])
+def prender():
+    print(request.json)
+    data = request.json
+    x = data['action']
+    arduino.write(bytes(x, 'utf-8')) 
+    time.sleep(1) 
+    data = arduino.readline() 
+    return data
+
+@app.route('/temperatura', methods = ['GET'])
+def getTemperatura():
+    datos = arduino.readline() 
+    print(datos)
+    return datos
 
 if __name__ == '__main__':
     app.run(debug=True, port=8002) #lunch server on port 5000
